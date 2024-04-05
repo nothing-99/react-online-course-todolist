293065f4d7fc4b19659142cf18ed2d0f0f11c9b8
Kim Sanghun <sanghun-kim@naver.com>
2024-04-06 00:12:24 +0900

    Study: Form
    - react-hook-form 라이브러리를 이용해 Form ( in react ) 을 쉽게 구현할 수 있다.
      - validation 내용만 작성하면 진행해주고, 에러가 난 input field 를 자동으로 포커싱
      - useState, validation 의 코드를 줄일 수 있다
      - handleSubmit 함수를 호출하면 validation 이 진행된다.
      - setError / validate (in register options) 를 이용한 custom validation 도 가능하다.
        - setError 를 통해 프론트 단이 아닌 서버에 의한 에러도 처리할 수 있다.
    
    - id / name
      - id는 유일한 값이어야 하며 css / js 단에서 <input> 을 식별할 때 사용
      - name은 유일한 값이 아니어도 되며 HTML-request 보낼 때 사용

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,97 +1,131 @@
-import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IFormValues {
+  userId: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  confirmed: string;
+}
 
 export default function TodoList() {
-  const [userId, setUserId] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
+  const { register, handleSubmit, formState, setError } =
+    useForm<IFormValues>();
 
-  const [isValidUserId, setIsValidUserId] = useState(true);
-  const [isValidEmail, setIsValidEmail] = useState(true);
-  const [isValidPhoneNumber, setIsValidPhoneNumber] = useState(true);
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    setIsValidUserId(validateUserId());
-    setIsValidEmail(validateEmail());
-    setIsValidPhoneNumber(validatePhoneNumber());
-  };
-  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
-    console.log("handleBlur 실행");
-    const {
-      currentTarget: { id },
-    } = e;
-    let isValid = true;
-
-    if (id === "userId") {
-      isValid = validateUserId();
-      setIsValidUserId(isValid);
-    } else if (id === "email") {
-      isValid = validateEmail();
-      console.log(isValid);
-    } else if (id === "phoneNumber") {
-      isValid = validatePhoneNumber();
-      setIsValidPhoneNumber(isValid);
+  const onSumibt: SubmitHandler<IFormValues> = (data) => {
+    const { password, confirmed } = data;
+    if (password !== confirmed) {
+      setError(
+        "confirmed",
+        { message: "비밀번호가 맞지 않습니다" },
+        { shouldFocus: true }
+      );
     }
   };
-  const validateUserId = () => !!userId.match(/^[\w]{4,20}$/);
-  const validateEmail = () => !!email.match(/[a-zA-Z0-9]+@[a-z]+\.com/);
-  const validatePhoneNumber = () => !!phoneNumber.match(/[0-9]{10,12}/);
 
+  console.log(formState.errors);
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSumibt)}>
+        {/* userId */}
         <div>
-          <label htmlFor="userId">
-            <div>
-              <input
-                id="userId"
-                type="text"
-                value={userId}
-                placeholder="영어, 숫자, _ 로 구성된 8자 이상 20자 이하로"
-                onChange={(e) => setUserId(e.currentTarget.value)}
-                onBlur={handleBlur}
-              />
-            </div>
+          <label>
+            <input
+              {...register("userId", {
+                required: {
+                  value: true,
+                  message: "아이디를 입력하세요",
+                },
+                pattern: {
+                  value: /^[\w]{4,20}$/,
+                  message:
+                    "4자 이상, 20자 이하로 입력하세요/알파벳, 숫자, _ 만으로 구성하세요",
+                },
+                validate: (v) =>
+                  !v.includes("fuck") || "fuck 단어를 포함하지 마세요",
+              })}
+              placeholder="아이디 (영문, 숫자, _ / 8자 이상 20자 이하)"
+            />
           </label>
           <div>
-            {isValidUserId || (
-              <span>영어, 숫자, _ 로 4자 이상 20자 이하로</span>
-            )}
+            <ul>
+              {formState.errors?.userId?.message?.split("/").map((v, i) => (
+                <li key={i}>{v}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        {/* email */}
+        <div>
+          <label>
+            <input
+              {...register("email", {
+                required: { value: true, message: "이메일을 입력하세요" },
+                pattern: {
+                  value: /^[a-zA-Z0-9]+@[a-zA-z0-9]+\.[a-zA-z0-9.]+$/,
+                  message: "올바른 이메일을 입력하세요",
+                },
+              })}
+              placeholder="이메일"
+            />
+          </label>
+          <div>
+            <span>{formState.errors?.email?.message}</span>
+          </div>
+        </div>
+        {/* phoneNumber */}
+        <div>
+          <label>
+            <input
+              {...register("phoneNumber", {
+                required: { value: true, message: "전화번호를 입력하세요" },
+                pattern: {
+                  value: /^[0-9]{10,12}$/,
+                  message: "- 없이 입력하세요",
+                },
+              })}
+              placeholder="전화번호 (- 없이)"
+            />
+          </label>
+          <div>
+            <span>{formState.errors?.phoneNumber?.message}</span>
+          </div>
+        </div>
+        {/* password */}
+        <div>
+          <label>
+            <input
+              {...register("password", {
+                required: { value: true, message: "비밀번호를 입력하세요" },
+                minLength: {
+                  value: 8,
+                  message: "최소 8자",
+                },
+              })}
+              placeholder="비밀번호 (8자 이상)"
+            />
+          </label>
+          <div>
+            <span>{formState.errors?.password?.message}</span>
+          </div>
+        </div>
+        {/* confirmed (password) */}
+        <div>
+          <label>
+            <input
+              {...register("confirmed", {
+                required: { value: true, message: "비밀번호를 입력하세요" },
+                minLength: { value: 8, message: "최소 8자" },
+              })}
+              placeholder="비밀번호 확인"
+            />
+          </label>
+          <div>
+            <span>{formState.errors?.confirmed?.message}</span>
           </div>
         </div>
         <div>
-          <label htmlFor="email">
-            <div>
-              <input
-                id="email"
-                type="email"
-                value={email}
-                placeholder="이메일을 입력하세요"
-                onChange={(e) => setEmail(e.currentTarget.value)}
-                onBlur={handleBlur}
-              />
-            </div>
-          </label>
-          <div>{isValidEmail || <span>올바른 이메일을 작성</span>}</div>
-        </div>
-        <div>
-          <label htmlFor="phoneNumber">
-            <div>
-              <input
-                id="phoneNumber"
-                type="text"
-                value={phoneNumber}
-                placeholder="-를 제외하고 입력하세요"
-                onChange={(e) => setPhoneNumber(e.currentTarget.value)}
-                onBlur={handleBlur}
-              />
-            </div>
-          </label>
-          <div>{isValidPhoneNumber || <span>- 은 제외하고 숫자만</span>}</div>
-        </div>
-        <div>
-          <button>Submit</button>
+          <button>Add</button>
         </div>
       </form>
     </div>
